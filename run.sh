f66863a6972b737d5eb80afa4b88ba9bec93ec28
Ahmed Zhran <ahmed.zhran.dev@gmail.com>
2026-06-05 23:14:29 +0300

    chore: set default port to 8100 in run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ cd "$(dirname "$0")"
 # Containerise all runtime data inside the repo
 export PAPERCLIP_HOME="$(pwd)/.paperclip-data"
 export PAPERCLIP_MIGRATION_AUTO_APPLY="true"
+export PORT="8100"
 
 echo "━━━ Paperclip ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo " Repo:  $(pwd)"
